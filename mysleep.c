31bbe51ac92342bfdf5229fee9b09936931cfb1e
inguuu <dlsrb8949@gmail.com>
2019-08-07 10:41:24 +0900

    commit mysleep.c

---
+++ mysleep.c
@@ -0,0 +1,57 @@
+/*
+ * mysleep.c
+ * 
+ * Copyright 2015 Jeong <jeong@jeong-VirtualBox>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+
+int i = 1;
+
+void loop(int signum)
+{
+	printf("Loop : %d\n",i);
+	i++;
+}
+
+void mysleep(int n)
+{
+	struct sigaction act;
+	act.sa_handler = loop;
+	sigaction(SIGALRM,&act,NULL);
+	alarm(n);
+	pause();
+}
+
+int main(int argc, char **argv)
+{
+	int j = 0;
+	while ( j < 10 )
+	{
+		mysleep(i);
+		j++;
+	}
+	
+	return 0;
+}
+

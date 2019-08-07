f5cf71fd843502fe511c1449be5a86ea17657c87
inguuu <dlsrb8949@gmail.com>
2019-08-07 10:42:26 +0900

    commit mycp.c

---
+++ mycp1.c
@@ -0,0 +1,51 @@
+/*
+ * mycp1.c
+ * 
+ * Copyright 2015 Jeong <jeong@jeong-VirtualBox>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main(int argc, char **argv)
+{
+	int fi, fo;
+	char buf[1024];
+	int rsize;
+	
+	fi = open(argv[1],O_RDONLY);
+	fo = open(argv[2],O_RDWR|O_CREAT|O_EXCL,0664);
+	
+	rsize = read(fi,buf,1024);
+	
+	while(read(fi,buf,1024) == 1024)
+	{
+		read(fi,buf,1024);
+		write(fo,buf,rsize);
+	}
+	close(fi);
+	close(fo);
+	return 0;
+}
+
